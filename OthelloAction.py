b11805307f832982c6ca9a8ff21aa4b97221e9e3
miuruna <miuraruna315@gmail.com>
2025-12-15 19:48:37 +0900

    Update Logger and Develop AI Memory
    CSV記録用クラスを修正
    計算に必要な値を保持するためのJSONファイル用のクラスを作成

--- OthelloAction.py
+++ OthelloAction.py
@@ -1,51 +1,91 @@
 import random
 import OthelloLogic
+from utils.ai_state_manager import AiStateManager
 from utils.game_logger import GameLogger
 
 # ロガーをグローバル変数として保持（プログラム実行中ずっと維持するため）
 _logger = None
 _my_color = None
 _opp_color = None
+_ai_memory = None
 
-def getAction(board, moves):
+def getAction(board:list, moves):
     global _logger
     global _my_color
     global _opp_color
-    
-    # 1. ロガーの初期化（初回のみ）
+    global _ai_memory
+
+    stone_count = stone_counter(board)
+
+    # 初回のみ実施される
     if _logger is None:
-        _logger = GameLogger(board)
-        _my_color, _opp_color = _logger.initial_save(board)
+        _logger = GameLogger()
+        if is_new(board):
+            _logger.finalize_previous_log()
+            _logger.create_new_log()
+            if stone_count == 4:
+                _my_color, _opp_color = "black", "white"
+            else:
+                _my_color, _opp_color = "white", "black"
+            _logger.initial_save(board, _my_color)
+
+    # 初回のみ実施される
+    if _ai_memory is None:
+        _ai_memory = AiStateManager()
+        if is_new(board):
+            if _my_color is None:
+                _my_color = "black" if stone_count % 2 == 0 else "white"
+            _ai_memory.new_game(board, _my_color)
+        else:
+            _ai_memory.continue_game()
+            _my_color = _ai_memory.game_info.my_color
+            if _my_color == "black":
+                _opp_color  = "white"
+            else:
+                _opp_color = "black"
+
+    # ひとつ前の盤面を取得
+    prev_board = _ai_memory.game_info.board
+
+    if stone_count != 4:
+        # 相手の打った手を取得
+        opp_move = get_opp_action(prev_board, board)
+        _logger.save(stone_count - 4, _opp_color, "MOVED", board, opp_move, _ai_memory.opponent_model)
+
+    _logger.save(stone_count - 3, _my_color, "THINKING", board, None, _ai_memory.opponent_model)
+
+    # 自分の手を決定する
+    next_move = random.choice(moves)
+
+    # 盤面を取得
+    next_board = OthelloLogic.execute(board, next_move, 1, len(board))
+
+    _logger.save(stone_count - 3, _my_color, "MOVED", next_board, next_move, _ai_memory.opponent_model)
+    
+    _ai_memory.game_info.board = next_board
+    _ai_memory.game_info.turn_count = stone_count - 3
+    _ai_memory.save_json()
+
+    return next_move
+
+def stone_counter(board:list):
+	count = 0
+	for row in board:
+		for cell in row:
+			if cell != 0:
+				count += 1
+	return count
+
+def is_new(board):
+	return not (stone_counter(board) > 5)
+
+def get_opp_action(prev_board, board):
+    if stone_counter(board) - stone_counter(prev_board) != 1:
+        opp_move = None
     else:
-        pass
-    
-    stone_count = sum(1 for row in board for cell in row if cell != 0)
-    current_step = stone_count - 4 + 1
-    
-	# 相手ターンの保存
-
-	# 差分を求める 相手が何を打ったのか
-    opp_action = [0, 0]
-    _logger.save(_step, _opp_color, "MOVED", board, opp_action)
-
-	# 
-	if _my_color == "white":
-		_step += 1
-    
-    # 3. 思考中のログ保存 (THINKING)
-    # Play.pyから渡されるboardは正規化(自分が1)されているためそのまま記録
-	_logger.save(_step, _my_color, "THINKING", board)
-
-    # --- AIの思考ロジック (ここは変更なし) ---
-	index = random.randrange(len(moves))
-	action = moves[index]
-    # -------------------------------------
-
-    # OthelloLogicを使って手を反映させる (playerは常に1)
-    # execute(board, action, player, size)
-	next_board = OthelloLogic.execute(board, action, 1, len(board))
-
-    # 5. 決定したログ保存 (MOVED)
-	_logger.save(_step, _my_color, "MOVED", next_board, action)
-
-	return action
+        for r in range(len(board)):
+            for c in range(len(board[r])):
+                if prev_board[r][c] == 0 and board[r][c] != 0:
+                    opp_move = [r, c]
+                    break
+    return opp_move
--- utils/bayes_player.py
+++ utils/bayes_player.py
@@ -1,3 +1,4 @@
 class BayesPlayer:
-    def __init__(self):
-        
+    def get_moves_probability(self, board, moves, evaluator_func):
+        for move in moves:
+            
